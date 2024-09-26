f6ad0f94273b8cd38b5932ae3472332c263aef4b
gpillon <giorgio.pillon@live.com>
2024-09-26 16:43:50 +0200

    init commit

--- packages/frontend/src/services/api.tsx
+++ packages/frontend/src/services/api.tsx
@@ -1,7 +1,30 @@
 import axios from 'axios';
 import { Vehicle, Refueling, NewVehicle, NewRefueling, LoginResponse, ApiResponse } from '../types';
+const SOCKET_SERVER_HOST: string = process.env.REACT_APP_SOCKET_HOST || 'localhost:3000';
 
-const API_URL = 'http://localhost:3000';
+interface BackendUrls {
+    socket_url: string;
+    api_url: string;
+}
+
+export const getBackendHost = (): BackendUrls => {
+    const socket_protocol: string = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+    const socket_url: string = process.env.PROD_BUILD === "true" 
+        ? `${socket_protocol}//${SOCKET_SERVER_HOST}` 
+        : `${socket_protocol}//localhost:3000`;
+
+    const api_url: string = process.env.PROD_BUILD === "true" 
+        ? `${window.location.protocol}//${window.location.host}` 
+        : `${window.location.protocol}//localhost:3000`;
+
+    return {
+        socket_url, 
+        api_url
+    };
+};
+
+
+const API_URL = getBackendHost().api_url;
 
 const api = axios.create({
   baseURL: API_URL,
