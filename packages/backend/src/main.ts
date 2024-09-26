e005a828f3dc89460af54d78ffbdb4eda4ad6964
gpillon <giorgio.pillon@live.com>
2024-09-26 17:38:44 +0200

    init commit

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -19,6 +19,13 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors({
+    origin: 'http://localhost:3001', // Your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
+  });
+  
   await app.listen(3000);
 }
 bootstrap();
